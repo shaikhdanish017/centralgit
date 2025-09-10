be8077b25635341a957d43e0d935fe34385ca3cf
shaikhdanish017 <89183272+shaikhdanish017@users.noreply.github.com>
2025-09-10 15:25:51 +0530

    Create HelloServlet.java

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello from Jenkins CI/CD WAR deployment!</h1>");
+    }
+}
